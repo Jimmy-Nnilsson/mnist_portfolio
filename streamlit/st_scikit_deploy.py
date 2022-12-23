e932a1ce8804f3a4c587293c0e87538bfef510b6
Jimmy Nilsson <jimmy.nilsson@hotmail.se>
2022-12-23 14:07:28 +0100

    test

--- streamlit/st_scikit_deploy.py
+++ streamlit/st_scikit_deploy.py
@@ -15,9 +15,9 @@ merge_map = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 
             19: 'T', 20: 'U', 21: 'V', 22: 'W', 23: 'X', 24: 'Y', 25: 'Z'}
 
 
-
-model_path = Path('./app/mnist_portfolio/streamlit/random_forest.joblib')
-nn_model_path = Path('./app/mnist_portfolio/streamlit/2emnist_save.h5')
+print(getcwd())
+model_path = Path('./streamlit/random_forest.joblib')
+nn_model_path = Path('./streamlit/2emnist_save.h5')
 
 
 model = joblib.load(model_path)   
